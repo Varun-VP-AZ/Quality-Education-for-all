01f494a6bece882128dab4475688aac71dde75d4
Varun-VP-AZ <varunpandiya.vp@gmail.com>
2023-08-10 19:26:08 +0530

    -a

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,19 +10,20 @@ import Home from "./pages/home";
 import Layout from "./components/layout";
 import Scholarship from "./pages/scholarship";
 import Login from "./pages/login";
-
 import Courses from "./pages/courses";
 import Products from "./pages/product";
+import Aboutus from "./pages/aboutus";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<Layout />}>
       <Route index element={<Home />} />
       <Route path="registration" element={<Signup />} />
-      <Route path="scholarship" element={<CardComponent />} />
+      <Route path="scholarship" element={<Scholarship />} />
       <Route path="login" element={<Login />} />
-      <Route path="courses" element={<Courses/>}/>
-      <Route path="market" element={<Products/>}/>
+      <Route path="courses" element={<Courses />} />
+      <Route path="market" element={<Products />} />
+      <Route path="/aboutus" element={<Aboutus />} />
     </Route>
   )
 );
